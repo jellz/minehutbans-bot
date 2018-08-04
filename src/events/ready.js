928145aeac3cfbb3a163bf7092429882e433b81e
Jellz <xdjellz@gmail.com>
2018-08-04 13:23:47 +1000

    add reminders

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,5 @@
 const run = module.exports = async () => {
     console.log('Logged in as ' + client.user.tag + '!');
     client.user.setActivity(client.config.activity || `with ${client.users.size} users.`);
+    require('../modules/reminderModule.js')();
 }
