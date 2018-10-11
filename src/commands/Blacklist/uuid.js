3c1ffbacaedb9bb5f0ca149b2ae27348abf422f4
Daniel Gulic <danielgulic@gmail.com>
2018-10-11 21:33:52 +1100

    uuid command

---
+++ src/commands/Blacklist/uuid.js
@@ -0,0 +1,17 @@
+const { MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
+const mcapi = require('mcapi');
+
+const run = module.exports.run = async (msg, args) => {
+  if (!args[0] || args[0].length > 16) return await client.invalidCommandUsage(msg, meta);
+  let uuid = await mcapi.usernameToUUID(args[0]);
+  let m = await msg.channel.send(uuid == 'fail' ? 'Invalid username' : uuid);
+  m.delete({ timeout: 15000 });
+}
+
+const meta = module.exports.meta = {
+  aliases: ['uuid'],
+  ownerOnly: false,
+  description: 'Get player UUID',
+  usage: '<player>'
+}
