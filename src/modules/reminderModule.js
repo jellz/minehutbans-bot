9e8200943e21170cb2f86d8c721bcb0907f0c937
jellz <danielgulic@gmail.com>
2019-04-23 15:26:39 +1000

    send reminders to meta channel

--- src/modules/reminderModule.js
+++ src/modules/reminderModule.js
@@ -3,7 +3,7 @@ module.exports = async (msg) => {
     if (process.env.MS_REMINDER_INTERVAL == -1) return;
     let unresolved = await r.table('cases').filter({ closed: { closed: false } }).run();
     if (unresolved.length < 1) return;
-    let channel = client.channels.get(process.env.DISCORD_CHANNEL_REPORTS);
+    let channel = client.channels.get(process.env.DISCORD_CHANNEL_REMINDERS);
     channel.send(`⚠ This is a courtesy reminder for <@&${process.env.DISCORD_ROLE_STAFF}>; cases ${unresolved.map(c => `**${c.id}**`).join(', ')} are unresolved.`);
   }, process.env.MS_REMINDER_INTERVAL);
 }
